6f4c451127e2c8f3c19ccc8c8ca7cab86b8f7764
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2020-09-29 20:05:47 +0200

    Updated AL_28_01

--- AL_28_01 - Piramidka/main.cpp
+++ AL_28_01 - Piramidka/main.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
 #include <string>
-#include <climits>
-#include <stdio.h>
-#include <ctype.h>
-#include <vector>
-#include <math.h>
-#include <utility>
-#include <algorithm>
-#include <array>
-#include <iterator>
 using namespace std;
 
-void piramidka(string wejscie, int lenght)
+int main()
 {
-    int a,b;
+    int lenght, a, b;
+    string wejscie;
+    cin.sync();
+    cin>>lenght;
+    cin.sync();
+    cin>>wejscie;
+
     a=b=lenght/2;
-    while(a>=0)
-    {
-        for(int j=0; j<lenght; j++)
-        {
-            if(j>=a&&j<=b)
-            {
+
+    while(a>=0) {
+        for(int j=0; j<lenght; j++) {
+            if( j>=a && j<=b ) {
                 cout<<wejscie[j];
-            }else{
+            } else {
                 cout<<".";
             }
         }
@@ -30,17 +25,8 @@ void piramidka(string wejscie, int lenght)
         a--;
         b++;
     }
-}
 
-int main()
-{
-    int lenght;
-    string wejscie;
-    cin>>lenght;
-    cin>>wejscie;
-    cin.ignore(INT_MAX, '\n');
-    piramidka(wejscie, lenght);
-
+    cin.sync();
     getchar();
     return 0;
 }
