2326e0f407bc60b496495a81ddec086e78afdfbe
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2020-10-27 21:54:49 +0100

    Zmieniono tabelę na wektor

--- CUK - Cukiernia/cuk.cpp
+++ CUK - Cukiernia/cuk.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <iterator>
+#include <vector>
 
 using namespace std;
 
@@ -90,7 +91,7 @@ int main()
     int n_gablot;
     long long int suma = 0;
     f_wej >> n_gablot;
-    gablota Gabloty[n_gablot];
+    vector <gablota> Gabloty;
     gablota* naj_drozdzowek = &Gabloty[0];
     gablota* naj_paczkow = &Gabloty[0];
     gablota* naj_rogalikow = &Gabloty[0];
@@ -98,13 +99,15 @@ int main()
     // Wczytanie zawartości gablot
     for(int i=0; i<n_gablot; i++)
     {
-        f_wej >> Gabloty[i].n_drozdzowek >> Gabloty[i].n_paczkow >> Gabloty[i].n_rogalikow;
+        gablota temp_wczytanie;
+        f_wej >> temp_wczytanie.n_drozdzowek >> temp_wczytanie.n_paczkow >> temp_wczytanie.n_rogalikow;
+        Gabloty.push_back(temp_wczytanie);
     }
 
     // Szukanie gabloty do których należy przeniesc ciastka
-    sort(Gabloty, Gabloty+n_gablot, sort_d);
-    sort(Gabloty+1, Gabloty+n_gablot, sort_p);
-    sort(Gabloty+2, Gabloty+n_gablot, sort_r);
+    sort(Gabloty.begin(), Gabloty.end(), sort_d);
+    sort(Gabloty.begin()+1, Gabloty.end(), sort_p);
+    sort(Gabloty.begin()+2, Gabloty.end(), sort_r);
 
     //sumowanie 3 pierwszych stacków
     for(int i=0; i<3; i++)
