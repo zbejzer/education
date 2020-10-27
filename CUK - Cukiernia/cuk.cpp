ae3cdf6b689b5cd1d1c5eb9aaa68373a8b84f6c0
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2020-10-27 18:02:33 +0100

    Zmieniono nazwę programu

--- CUK - Cukiernia/cuk.cpp
+++ CUK - Cukiernia/cuk.cpp
@@ -19,13 +19,32 @@ class gablota
         id = global_id;
         global_id++;
     }
+
+    bool isSingleType()
+    {
+        bool warunek = false;
+        int counter = 0;
+
+        if(n_drozdzowek == 0)
+            counter++;
+        if(n_paczkow == 0)
+            counter++;
+        if(n_rogalikow == 0)
+            counter++;
+        
+        if(counter >= 2){
+            warunek = true;
+        }
+
+        return warunek;
+    }
 };
 
 bool isGreater(gablota a, gablota b)
 {
     bool warunek = false;
 
-    cout << "Compare: " << a.id << " " << b.id << endl;
+    //cout << "Compare: " << a.id << " " << b.id << endl;
 
     if(a.n_paczkow + a.n_rogalikow < b.n_paczkow + b.n_rogalikow)
     {
@@ -45,7 +64,7 @@ bool isGreater(gablota a, gablota b)
 
 int main()
 {
-    int n_gablot;
+    int n_gablot, suma = 0;
     cin >> n_gablot;
     gablota Gabloty[n_gablot];
     gablota* naj_drozdzowek = &Gabloty[0];
@@ -59,14 +78,47 @@ int main()
     }
 
     // Szukanie gabloty do których należy przeniesc ciastka
-    cout << "Szukanie gabloty do których należy przeniesc ciastka" << endl;
+    //cout << "Sortowanie gablot" << endl;
     sort(Gabloty, Gabloty+n_gablot, isGreater);
 
+    //cout << endl;
+    //sumowanie drozdzowek
+    for(int i=0; i<n_gablot; i++)
+    {
+        if(i != 0 && !Gabloty[i].isSingleType() )
+        {
+            suma += Gabloty[i].n_drozdzowek;
+        }
+        //cout << suma << endl;
+    }
+    //cout << endl;
+    //sumowanie paczkow
+    for(int i=0; i<n_gablot; i++)
+    {
+        if(i != 1 && !Gabloty[i].isSingleType() )
+        {
+            suma += Gabloty[i].n_paczkow;
+        }
+        //cout << suma << endl;
+    }
+    //cout << endl;
+    //sumowanie rogalikow
+    for(int i=0; i<n_gablot; i++)
+    {
+        if(i != 2 && !Gabloty[i].isSingleType() )
+        {
+            suma += Gabloty[i].n_rogalikow;
+        }
+        //cout << suma << endl;
+    }
+    //cout << endl;
+
     // Wypisanie gablot
-    cout << "Wypisanie top gablot" << endl;
     for(int i=0; i<n_gablot; i++){
         cout << Gabloty[i].n_drozdzowek << " " << Gabloty[i].n_paczkow << " " << Gabloty[i].n_rogalikow << "  id: " << Gabloty[i].id << endl;
     }
+
+    cout << suma << endl;
 
     getchar();
     getchar();
