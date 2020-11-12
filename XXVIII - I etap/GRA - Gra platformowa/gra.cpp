4a65ae1c3bf23d0c5aadf9a15f1e4efa2ff484d1
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2020-11-13 00:03:25 +0100

    Brak optymalizacji

--- XXVIII - I etap/GRA - Gra platformowa/gra.cpp
+++ XXVIII - I etap/GRA - Gra platformowa/gra.cpp
@@ -32,9 +32,9 @@ class Poziom
         for(int i=0; i<platformy.size(); i++){
             for(int j=1; j<=dlugosc_platform; j++){
                 if(platformy[i].dziury.count(j) != 0){
-                    cout << "  ";
+                    cout << " ";
                 }else{
-                    cout << "- ";
+                    cout << "-";
                 }
             }
             cout << endl;
@@ -44,71 +44,71 @@ class Poziom
 
     void znajdz_droge(int start_x, int start_y, int start_droga, int *min_droga)
     {   
-        //cout << "Szukanie drogi na x: " << start_x << " y: " << start_y << " obecna droga " << start_droga << endl;
-        if( start_y == 1 ) {                              // Górne piętro
-            while(
-                // y-1 jako y; vector liczy platformy od 0
-                platformy[start_y-1].dziury.count(start_x+1) == 0 &&    // platforma na y, x+1
-                start_x < dlugosc_platform                              // nie jest końcem
-            ){
-                start_x++;
-            }
-            
-            if (platformy[start_y-1].dziury.count(start_x+1) != 0){         // dziura przed
-                znajdz_droge(start_x+2, start_y, start_droga+1, min_droga);
-                znajdz_droge(start_x+1, start_y+1, start_droga, min_droga);
-            }
-            if (start_x == dlugosc_platform && start_droga < *min_droga){    // koniec drogi
-                *min_droga = start_droga;
-                cout << "Koniec! " << start_x << " " << start_y << endl;
-            }
-        } else if ( start_y == platformy.size() ) {     // Dolne piętro
-            while(
-                // y-1 jako y; vector liczy platformy od 0
-                platformy[start_y-1].dziury.count(start_x+1) == 0 &&    // platforma na y, x+1
-                platformy[start_y-2].dziury.count(start_x) == 0 &&      // platforma na y-1, x
-                start_x < dlugosc_platform                              // nie jest końcem
-            ){
-                start_x++;
-            }
-            
-            if (platformy[start_y-1].dziury.count(start_x+1) != 0){         // dziura przed
-                znajdz_droge(start_x+2, start_y, start_droga+1, min_droga);
-            }
-            if (platformy[start_y-2].dziury.count(start_x) != 0){           // dziura nad
-                znajdz_droge(start_x+1, start_y-1, start_droga+1, min_droga);
-                if (platformy[start_y-1].dziury.count(start_x+1) == 0){
-                    znajdz_droge(start_x+1, start_y, start_droga, min_droga);
+        // Optymalizacja
+        if(start_droga < *min_droga){
+            //cout << "Szukanie drogi na x: " << start_x << " y: " << start_y << " obecna droga " << start_droga << endl;
+            if( start_y == 1 ) {                              // Górne piętro
+                while(
+                    // y-1 jako y; vector liczy platformy od 0
+                    platformy[start_y-1].dziury.count(start_x+1) == 0 &&    // platforma na y, x+1
+                    start_x < dlugosc_platform                              // nie jest końcem
+                ){
+                    start_x++;
                 }
-            }
-            if (start_x == dlugosc_platform && start_droga < *min_droga){    // koniec drogi
-                *min_droga = start_droga;
-                cout << "Koniec! " << start_x << " " << start_y << endl;
-            }
                 
-        } else {                                        // Piętra pomiędzy
-            while(
-                // y-1 jako y; vector liczy platformy od 0
-                platformy[start_y-1].dziury.count(start_x+1) == 0 &&    // platforma na y, x+1
-                platformy[start_y-2].dziury.count(start_x) == 0 &&      // platforma na y-1, x
-                start_x < dlugosc_platform                              // nie jest końcem
-            ){
-                start_x++;
-            }
-            
-            if (platformy[start_y-1].dziury.count(start_x+1) != 0){         // dziura przed
-                znajdz_droge(start_x+2, start_y, start_droga+1, min_droga);
-                znajdz_droge(start_x+1, start_y+1, start_droga, min_droga);
-            }
-            if (platformy[start_y-2].dziury.count(start_x) != 0){           // dziura nad
-                znajdz_droge(start_x+1, start_y-1, start_droga+1, min_droga);
-                if (platformy[start_y-1].dziury.count(start_x+1) == 0){
-                    znajdz_droge(start_x+1, start_y, start_droga, min_droga);
+                if (platformy[start_y-1].dziury.count(start_x+1) != 0){         // dziura przed
+                    znajdz_droge(start_x+2, start_y, start_droga+1, min_droga);
+                    znajdz_droge(start_x+1, start_y+1, start_droga, min_droga);
                 }
-            }
-            if (start_x == dlugosc_platform && start_droga < *min_droga){    // koniec drogi
-                *min_droga = start_droga;
-                cout << "Koniec! " << start_x << " " << start_y << endl;
+                if (start_x == dlugosc_platform && start_droga < *min_droga){    // koniec drogi
+                    *min_droga = start_droga;
+                }
+            } else if ( start_y == platformy.size() ) {     // Dolne piętro
+                while(
+                    // y-1 jako y; vector liczy platformy od 0
+                    platformy[start_y-1].dziury.count(start_x+1) == 0 &&    // platforma na y, x+1
+                    platformy[start_y-2].dziury.count(start_x) == 0 &&      // platforma na y-1, x
+                    start_x < dlugosc_platform                              // nie jest końcem
+                ){
+                    start_x++;
+                }
+                
+                if (platformy[start_y-1].dziury.count(start_x+1) != 0){         // dziura przed
+                    znajdz_droge(start_x+2, start_y, start_droga+1, min_droga);
+                }
+                if (platformy[start_y-2].dziury.count(start_x) != 0){           // dziura nad
+                    if (platformy[start_y-1].dziury.count(start_x+1) == 0){
+                        znajdz_droge(start_x+1, start_y, start_droga, min_droga);
+                    }
+                    znajdz_droge(start_x+1, start_y-1, start_droga+1, min_droga);
+                }
+                if (start_x == dlugosc_platform && start_droga < *min_droga){    // koniec drogi
+                    *min_droga = start_droga;
+                }
+                    
+            } else {                                        // Piętra pomiędzy
+                while(
+                    // y-1 jako y; vector liczy platformy od 0
+                    platformy[start_y-1].dziury.count(start_x+1) == 0 &&    // platforma na y, x+1
+                    platformy[start_y-2].dziury.count(start_x) == 0 &&      // platforma na y-1, x
+                    start_x < dlugosc_platform                              // nie jest końcem
+                ){
+                    start_x++;
+                }
+                
+                if (platformy[start_y-1].dziury.count(start_x+1) != 0){         // dziura przed
+                    znajdz_droge(start_x+2, start_y, start_droga+1, min_droga);
+                    znajdz_droge(start_x+1, start_y+1, start_droga, min_droga);
+                }
+                if (platformy[start_y-2].dziury.count(start_x) != 0){           // dziura nad
+                    if (platformy[start_y-1].dziury.count(start_x+1) == 0){
+                        znajdz_droge(start_x+1, start_y, start_droga, min_droga);
+                    }
+                    znajdz_droge(start_x+1, start_y-1, start_droga+1, min_droga);
+                }
+                if (start_x == dlugosc_platform && start_droga < *min_droga){    // koniec drogi
+                    *min_droga = start_droga;
+                }
             }
         }
     }
@@ -146,7 +146,7 @@ int main(){
         gra.platformy.push_back(temp_platforma);
     }
 
-    gra.wypisz();
+    //gra.wypisz();
 
     // Zapytania
     for(int i = 0; i < n_zapytan; i++)
