b1e63291ac833f91cc2a3c6706d374cc591e754f
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2020-11-24 00:29:27 +0100

    Sumowanie prawej strony

--- Inne/Grzybiarz/grzyb.cpp
+++ Inne/Grzybiarz/grzyb.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main()
 {
-    int n_mushrooms, start_position, moves, max_left, max_right, maximal_sum = 0;
+    int n_mushrooms, start_position, moves, max_left, max_right, maximal_sum;
     vector <int> mushrooms;
     vector <int> prefix_sum = {0};
 
@@ -41,6 +41,8 @@ int main()
         prefix_sum.push_back( prefix_sum[i] + mushrooms[i] );
     }
 
+    maximal_sum = mushrooms[start_position];
+
     for(int i=0; i<=n_mushrooms; i++) cout << i << '\t';
     cout << endl;
     for(int i=0; i<n_mushrooms; i++) cout << mushrooms[i] << '\t';
@@ -62,39 +64,16 @@ int main()
         cout << "\t\tSuma: " << new_sum << endl;
     }
 
-    /*for(int i=max_right; i > start_position; i--)
+    for(int i=max_right; i > start_position; i--)
     {
-        int current_end = min( start_position, start_position + moves - (start_position-i)*2 );
-        current_end = min( current_end, n_mushrooms-1);
-        int new_sum = prefix_sum[i + 1] - prefix_sum[i];
+        int current_start = min( start_position, start_position - (moves - (i-start_position)*2) );
+        current_start = max( current_start, 0);
+        int new_sum = prefix_sum[i + 1] - prefix_sum[current_start];
         if(new_sum > maximal_sum) maximal_sum = new_sum;
 
-        cout << "\tSumowanie od pozycji " << i << " do " << current_end << endl;
+        cout << "\tSumowanie od pozycji " << current_start << " do " << i << endl;
         cout << "\t\tSuma: " << new_sum << endl;
-    }*/
-
-    /*
-    suma = abs(S_pre[pozycja_g+1] - S_pre[0]);
-    if(suma>najw_suma) najw_suma=suma;
-
-    suma = abs(S_pre[6+1] - S_pre[pozycja_g]);
-    if(suma>najw_suma) najw_suma=suma;
-
-    for(int i=pozycja_g - (m/2); (pozycja_g + (m - (pozycja_g-i)*2)) < grzyby.size(); i++)
-    {
-        suma = S_pre[ pozycja_g + (m - (pozycja_g-i)*2) +1 ] - S_pre[i];
-        if(suma>najw_suma) najw_suma=suma;
-
-        cout << "Sumowanie od pozycji " << i << " do " << pozycja_g + (m - (pozycja_g-i)*2) << "\tSuma: " << suma << endl;
     }
-
-    for(int i=6; pozycja_g - (m - (i-pozycja_g)*2) >= 0; i--)
-    {
-        suma = S_pre[i+1] - S_pre[ pozycja_g - (m - (i-pozycja_g)*2) ];
-        if(suma>najw_suma) najw_suma=suma;
-
-        cout << "Sumowanie od pozycji " << i << " do " << pozycja_g - (m - (i-pozycja_g)*2) << "\tSuma: " << suma << endl;
-    }*/
     
     cout << maximal_sum << endl;
 
