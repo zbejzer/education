8897f5ee632641626ad2f3b1530090b881828e58
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2021-03-15 21:21:26 +0100

    Szkic programu Operacja Bitenshteina

---
+++ LIGA algorytmiczna CMI - Gremliny/OPE.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include<algorithm>
+
+using namespace std;
+
+int roznica(string haslo1, string haslo2, int wynik)
+{
+    if(haslo2.size() > haslo1.size())
+        swap(haslo1, haslo2);
+
+    for(int i=0; i<haslo1.size(); i++)
+    {
+        if( haslo1[i] == haslo2[i] )
+        {
+            haslo1.erase(haslo1.begin() + i);
+            haslo2.erase(haslo2.begin() + i);
+            wynik++;
+            i--;
+        }
+    }
+    
+    cout << haslo1 << " " << haslo2 << endl;
+
+    return wynik;
+}
+
+
+int main()
+{
+    // basic roznica function driver
+    string wyraz1, wyraz2;
+    int n;
+
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> wyraz1 >> wyraz2;
+        cout << roznica(wyraz1, wyraz2, 0);
+    }
+    
+    return 0;
+}
