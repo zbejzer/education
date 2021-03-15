76ed5cf78da50dd3fc75f85a03d5e2c8e4210ecd
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2021-03-15 22:29:07 +0100

    Ukończenie głównej części programu

--- LIGA algorytmiczna CMI - Gremliny/OPE.cpp
+++ LIGA algorytmiczna CMI - Gremliny/OPE.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <utility>
 #include <cmath>
-#include<algorithm>
+#include <algorithm>
+#include <map>
+#include <array>
+#include <numeric>
 
 using namespace std;
 
@@ -23,27 +25,56 @@ int roznica(string haslo1, string haslo2)
             i--;
         }
     }
-    
-    cout << haslo1 << " " << haslo2 << " ";
 
     wynik = max(haslo1.size(), haslo2.size());
+
+    cout << haslo1 << " " << haslo2 << " " << wynik << endl;
+
     return wynik;
 }
 
+bool isAlphabetical(string nazwa1, string nazwa2)
+{
+    for(int i=0; i<min(nazwa1.size(), nazwa2.size()); i++)
+        if(nazwa1[i] < nazwa2[i])
+            return true;
+        else if(nazwa1[i] > nazwa2[i])
+            return false;
+
+    if(nazwa1.size() < nazwa2.size())
+        return true;
+    else
+        return false;
+}
 
 int main()
 {
-    // basic roznica function driver
+    vector <array<string, 2>> uzytkownicy;
+    vector <array<string, 2>> najmniejPodobone;
+    vector <array<string, 2>> najbardziejPodobone;
+    int najmniejszaRoznica = numeric_limits<int>::max();
+    int najwieksza = numeric_limits<int>::min();
+
     string wyraz1, wyraz2;
     int n;
 
-    cin >> n;
+    cin >> n;    
 
+    // Wczytanie uzytkownikow
     for(int i=0; i<n; i++)
     {
-        cin >> wyraz1 >> wyraz2;
-        cout << roznica(wyraz1, wyraz2) << endl;
+        string uzytkownik, haslo;
+        cin >> uzytkownik >> haslo;
+
+        uzytkownicy[i][0] = uzytkownik;
+        uzytkownicy[i][1] = haslo;
     }
     
+    // Szukanie najmniej i najbardziej podobnych haseł
+    for(int i=0; i<n; i++)
+    {
+        
+    }
+
     return 0;
 }
