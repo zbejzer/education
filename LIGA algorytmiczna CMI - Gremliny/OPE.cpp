f17b12a8d973576efdcc9528930563d6b720c57c
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2021-03-16 19:16:46 +0100

    Ukończenie Operacja Bitenshteina

--- LIGA algorytmiczna CMI - Gremliny/OPE.cpp
+++ LIGA algorytmiczna CMI - Gremliny/OPE.cpp
@@ -28,7 +28,7 @@ int roznica(string haslo1, string haslo2)
 
     wynik = max(haslo1.size(), haslo2.size());
 
-    cout << haslo1 << " " << haslo2 << " " << wynik << endl;
+    //cout << haslo1 << " " << haslo2 << " " << wynik << endl;
 
     return wynik;
 }
@@ -49,16 +49,17 @@ bool isAlphabetical(string nazwa1, string nazwa2)
 
 int main()
 {
-    vector <array<string, 2>> uzytkownicy;
-    vector <array<string, 2>> najmniejPodobone;
-    vector <array<string, 2>> najbardziejPodobone;
+    vector <string> najmniejPodobone;
+    vector <string> najbardziejPodobone;
     int najmniejszaRoznica = numeric_limits<int>::max();
-    int najwieksza = numeric_limits<int>::min();
+    int najwiekszaRoznica = numeric_limits<int>::min();
 
     string wyraz1, wyraz2;
     int n;
 
-    cin >> n;    
+    cin >> n;
+
+    vector <array<string, 2>> uzytkownicy(n);
 
     // Wczytanie uzytkownikow
     for(int i=0; i<n; i++)
@@ -69,12 +70,51 @@ int main()
         uzytkownicy[i][0] = uzytkownik;
         uzytkownicy[i][1] = haslo;
     }
-    
+
     // Szukanie najmniej i najbardziej podobnych haseł
-    for(int i=0; i<n; i++)
+    // Zaadaptowany bubble sort
+    for(int i=0; i<n-1; i++)
     {
-        
+        for(int j=i+1; j<n; j++)
+        {
+            //cout << i << " -> " << j << endl;
+            int obecnaRoznica = roznica(uzytkownicy[i][1], uzytkownicy[j][1]);
+
+            if(obecnaRoznica >= najwiekszaRoznica)
+            {
+                array<string, 2> t = { uzytkownicy[i][0], uzytkownicy[j][0] };
+
+                if( !isAlphabetical(t[0], t[1]) ) swap(t[0], t[1]);
+                if(obecnaRoznica > najwiekszaRoznica) najmniejPodobone.clear();
+
+                najmniejPodobone.push_back(t[0] + " " + t[1]);
+                najwiekszaRoznica = obecnaRoznica;
+            }
+
+            if(obecnaRoznica <= najmniejszaRoznica)
+            {
+                array<string, 2> t = { uzytkownicy[i][0], uzytkownicy[j][0] };
+
+                if( !isAlphabetical(t[0], t[1]) ) swap(t[0], t[1]);
+                if(obecnaRoznica < najmniejszaRoznica) najbardziejPodobone.clear();
+
+                najbardziejPodobone.push_back(t[0] + " " + t[1]);
+                najmniejszaRoznica = obecnaRoznica;
+            }
+        }
     }
+
+    // Sortowanie par w wektorach
+    sort(najbardziejPodobone.begin(), najbardziejPodobone.end(), isAlphabetical);
+    sort(najmniejPodobone.begin(), najmniejPodobone.end(), isAlphabetical);
+
+    cout << najmniejszaRoznica << endl;
+    for(int i=0; i<najbardziejPodobone.size(); i++)
+        cout << najbardziejPodobone[i] << endl;
+
+    cout << najwiekszaRoznica << endl;
+    for(int i=0; i<najmniejPodobone.size(); i++)
+        cout << najmniejPodobone[i] << endl;
 
     return 0;
 }
