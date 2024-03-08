2b5cb76bfde25fae7833d816747883f950c98085
Stanislaw Borodziuk <borodziukstanislaw@gmail.com>
2024-03-08 05:03:22 +0100

    Tests for second task

--- studia-PO-Lab-2/main.cpp
+++ studia-PO-Lab-2/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include "Library.h"
 
 using namespace std;
 
@@ -20,8 +21,25 @@ int main() {
 	cout << "e: " << e << " b2:" << b2 << endl;
 	e.setAuthor("Juliusz Slowacki");
 	cout << "e: " << e << endl;
-	e.setTitle("Dziady");
+	e.setTitle("Kordian");
 	cout << "e: " << e << endl;
+
+	Library l;
+	cout << "l: " << l << endl;
+	//3-5 ksi¹¿ek
+	Library l1 = { {"Fiodor Dostojewski", "Zbrodnia i Kara"},
+	{"Sun Tzu", "Sztuka Wojny"},
+	{"J.R.R. Tolkien", "Wladca Pierscieni"} };
+	cout << "l1: " << l1 << endl;
+	Library l2(2);
+	cout << "l2: " << l2 << endl;
+	l2[0] = { "Frank Herbert", "Diuna" };
+	l2[1] = { "Joseph Conrad", "Jadro Ciemnosci" };
+	cout << "l2: " << l2 << endl;
+	l = std::move(l2);
+	cout << "l: " << l << " l2: " << l2 << endl;
+	l1[0] = std::move(l[1]);
+	cout << "l1: " << l1 << " l: " << l << endl;
 
 	return 0;
 }
