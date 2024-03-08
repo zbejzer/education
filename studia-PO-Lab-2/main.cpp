10da584713c8a9728677ae4e519a6f8da982cda1
Stanislaw Borodziuk <borodziukstanislaw@gmail.com>
2024-03-08 03:59:09 +0100

    Change example data

--- studia-PO-Lab-2/main.cpp
+++ studia-PO-Lab-2/main.cpp
@@ -7,20 +7,20 @@ using namespace std;
 
 int main() {
 
-	string a = "<?>", t = "<?>";
+	string a = "Henryk Sienkiewicz", t = "Ogniem i mieczem";
 	Book e;
 	cout << "e: " << e << endl;
 	Book b1 = { a, t };
 	cout << "b1: " << b1 << endl;
-	Book b2 = { "<?>","<?>" };
+	Book b2 = { "Adam Mickiewicz","Dziady" };
 	cout << "b2: " << b2 << endl;
 	Book b3 = b1;
 	cout << "b3: " << b3 << " b1: " << b1 << endl;
 	e = std::move(b2);
 	cout << "e: " << e << " b2:" << b2 << endl;
-	e.setAuthor("<?>");
+	e.setAuthor("Juliusz Slowacki");
 	cout << "e: " << e << endl;
-	e.setTitle("<?>");
+	e.setTitle("Dziady");
 	cout << "e: " << e << endl;
 
 	return 0;
