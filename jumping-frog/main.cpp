bf3c3ac9c0d869c6128c56db9b211bdd3b99ab20
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2024-11-02 15:02:36 +0100

    Add basic project structure

---
+++ jumping-frog/main.cpp
@@ -0,0 +1,17 @@
+#include <ncurses/ncurses.h>
+
+int main()
+{
+    initscr();
+    cbreak();
+    noecho();
+
+    mvaddch(0, 0, '+');
+    mvaddch(LINES - 1, 0, '-');
+    mvaddstr(10, 30, "press any key to quit");
+    refresh();
+
+    getch();
+
+    endwin();
+}
