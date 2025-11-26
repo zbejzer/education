f6451506ec9a85e3320fb5edf69bdd0712b5e96c
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2025-11-26 12:45:26 +0100

    feat: add latex generation

--- src/render.c
+++ src/render.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "common.h"
 #include "render.h"
@@ -13,4 +14,28 @@ void RenderTxt(FILE *file)
 
 void RenderPdf(FILE *file)
 {
+    time_t raw_time = time(NULL);
+    struct tm *time_info = localtime(&raw_time);
+
+    fprintf(file, "\\documentclass{article}\n"
+                  "\\usepackage{polski}\n"
+                  "\\title{Stan Magazynu}\n"
+                  "\\date{");
+    fprintf(file, "%d:%d %d/%d/%d", time_info->tm_hour, time_info->tm_min, time_info->tm_mday, time_info->tm_mon + 1,
+            time_info->tm_year + 1900);
+    fprintf(file, "}\n"
+                  "\\begin{document}\n"
+                  "\\maketitle\n"
+                  "\\begin{tabular}{|l|l|l|}\n"
+                  "\\hline\n"
+                  "ID & Nazwa & Ilość \\\\ \\hline\n");
+
+    for (int i = 0; i < product_count; i++)
+    {
+        fprintf(file, "%s & %s & %d \\\\\n", products[i].id, products[i].name, products[i].stock);
+    }
+
+    fprintf(file, "\\hline\n"
+                  "\\end{tabular}\n"
+                  "\\end{document}\n");
 }
