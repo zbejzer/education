b3862a7cdfec75e091407b08cdbc7dbc6cc09b89
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2026-01-26 00:29:35 +0100

    feat: error handling in run_scenario.sh

--- tools/run_scenario.sh
+++ tools/run_scenario.sh
@@ -1,37 +1,65 @@
 #!/usr/bin/env bash
 
 mode=$1
+scenario_file=$2
+output_dir=$3
 
-cd "$(dirname "$0")"
-cd "./../"
-
-work_dir="$(realpath "./")"
-build_dir="$work_dir/build"
-test_dir="$work_dir/data/scenario01"
-dist_dir="$work_dir/dist"
+warehouse_bin="$(realpath $(dirname "$0"))/warehouse"
+scenario_dir="$(realpath $(dirname "$scenario_file"))/"
 
 exit_code=0
 
-mkdir -p "$dist_dir/" 2>/dev/null
+if [ -z "$mode" ]; then
+    echo "Mode not specified"
+    exit_code=1
+fi
 
-cd "$test_dir"
+if [ -z "$scenario_file" ]; then
+    echo "Input file not specified"
+    exit_code=1
+fi
 
-"$build_dir/warehouse" $mode < scenario.txt | while
-    read -r filename
-do
-    mv "$test_dir/$filename" "$dist_dir"
-    cd "$dist_dir"
-    if [ "$mode" == "pdf" ]; then
+if [ -z "$output_dir" ]; then
+    echo "Output directory not specified"
+    exit_code=1
+fi
+
+if [ ! -f "$scenario_file" ]; then
+    echo "Specified scenario file $scenario_file doesn't exist"
+    exit_code=1
+fi
+
+if [ "$exit_code" -ne "0" ]; then
+    echo "Usage: $(basename $0) [mode] [input file] [output directory]"
+    exit $exit_code
+fi
+
+if [ ! -d "$output_dir" ]; then
+    mkdir -p "$output_dir/" || exit 1
+fi
+
+scenario_file="$(realpath $scenario_file)"
+output_dir="$(realpath $output_dir)"
+
+cd "$scenario_dir"
+
+"$warehouse_bin" "$mode" < "$scenario_file" | while read -r filename; do
+    cd "$scenario_dir"
+    mv "./$filename" "$output_dir"
+    cd "$output_dir"
+    if [ "$mode" = "pdf" ]; then
         out_filename="$(basename "$filename" ".tex").pdf"
     else
         out_filename="$filename"
     fi
-    echo "Creating $dist_dir/$out_filename"
-    if [ "$mode" == "pdf" ]; then
-        pdflatex -interaction=nonstopmode "$dist_dir/$filename" >/dev/null || (echo "Error creating $dist_dir/$filename" && exit_code=73)
+    echo "Creating $output_dir/$out_filename"
+    if [ "$mode" = "pdf" ]; then
+        pdflatex -interaction=nonstopmode "$output_dir/$filename" >/dev/null || (echo "Error creating $output_dir/$filename" && exit_code=73)
     fi
 done
 
-mv "$test_dir/store.txt" "$dist_dir"
+exit_code=${PIPESTATUS[0]}
+
+mv "$scenario_dir/store.txt" "$output_dir"
 
 exit $exit_code
