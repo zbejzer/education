e6aca22c6edfde742e78cf318c0539385706d55b
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2026-03-12 23:27:30 +0100

    refactor: add type hints and docstrings

--- exercism/guidos-gorgeous-lasagna/__init__.py
+++ exercism/guidos-gorgeous-lasagna/__init__.py
@@ -1,12 +1,31 @@
-EXPECTED_BAKE_TIME = 40
+EXPECTED_BAKE_TIME: int = 40
+PREPARATION_TIME: int = 2
 
-def bake_time_remaining(minutes_passed)
-    return EXPECTED_BAKE_TIME - minutes_passed
 
-def preparation_time_in_minutes(number_of_layers)
-    return number_of_layers * 2
+def bake_time_remaining(elapsed_bake_time: int) -> int:
+    """Calculate the bake time remaining.
 
-def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time)
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+
+def preparation_time_in_minutes(number_of_layers: int) -> int:
+    """Calculate the preparation time.
+
+    :param number_of_layers: int - the number of layers in the lasagna.
+    :return: int - preparation time (in minutes) derived from 'PREPARATION_TIME'.
+    """
+
+    return number_of_layers * PREPARATION_TIME
+
+
+def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int) -> int:
     """Calculate the elapsed cooking time.
 
     :param number_of_layers: int - the number of layers in the lasagna.
