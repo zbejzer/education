92e58f9381feb4422fc44305c0343837d198cfdc
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2026-03-12 23:32:13 +0100

    refactor: add type hints and docstrings

--- exercism/currency-exchange/__init__.py
+++ exercism/currency-exchange/__init__.py
@@ -1,14 +1,53 @@
-def exchange_money(budget, exchange_rate)
+def exchange_money(budget: float, exchange_rate: float) -> float:
+    """
+
+    :param budget: float - amount of money you are planning to exchange.
+    :param exchange_rate: float - unit value of the foreign currency.
+    :return: float - exchanged value of the foreign currency you can receive.
+    """
+
     return budget / exchange_rate
 
-def get_change(budget, exchanging_value)
+
+def get_change(budget: float, exchanging_value: float) -> float:
+    """
+
+    :param budget: float - amount of money you own.
+    :param exchanging_value: float - amount of your money you want to exchange now.
+    :return: float - amount left of your starting currency after exchanging.
+    """
+
     return budget - exchanging_value
 
-def get_value_of_bills(denomination, number_of_bills)
+
+def get_value_of_bills(denomination: int, number_of_bills: int) -> int:
+    """
+
+    :param denomination: int - the value of a bill.
+    :param number_of_bills: int - total number of bills.
+    :return: int - calculated value of the bills.
+    """
+
     return denomination * number_of_bills
 
-def get_number_of_bills(amount, denomination)
+
+def get_number_of_bills(amount: float, denomination: int) -> int:
+    """
+
+    :param amount: float - the total starting value.
+    :param denomination: int - the value of a single bill.
+    :return: int - number of bills that can be obtained from the amount.
+    """
+
     return amount // denomination
 
-def get_leftover_of_bills(amount, denomination)
+
+def get_leftover_of_bills(amount: float, denomination: int) -> float:
+    """
+
+    :param amount: float - the total starting value.
+    :param denomination: int - the value of a single bill.
+    :return: float - the amount that is "leftover", given the current denomination.
+    """
+
     return amount % denomination
